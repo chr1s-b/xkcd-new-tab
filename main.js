04968b6b30503182d4d5d834bc2c4508783b21db
Chris <cboddy1@hotmail.com>
2019-12-20 17:17:07 +0000

    Add alt text. Res Issue #1

--- main.js
+++ main.js
@@ -2,9 +2,12 @@
     response.json().then(function(data) {
       var img = data.img;
       var title = data.safe_title;
+	  var alt = data.alt;
       var targetTitle = document.getElementById('title');
       var targetImg = document.getElementById('xkcd');
+	  var targetAlt = document.getElementById('alt');
       targetImg.src = img;
       targetTitle.innerText = title;
+	  targetAlt.innerText = alt;
     });
   });
