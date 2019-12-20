adc24392ab9fdbb6c9554f0936a2e6158f945aca
Chris <cboddy1@hotmail.com>
2019-12-20 17:32:30 +0000

    Add comic issue num

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
       var img = data.img;
       var title = data.safe_title;
 	  var alt = data.alt;
+	  var num = data.num;
       var targetTitle = document.getElementById('title');
       var targetImg = document.getElementById('xkcd');
 	  var targetAlt = document.getElementById('alt');
+	  var targetNum = document.getElementById('num');
       targetImg.src = img;
       targetTitle.innerText = title;
 	  targetAlt.innerText = alt;
+	  targetNum.innerText = "XKCD #"+num.toString();
     });
   });
